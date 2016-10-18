de4506f221181e31196583ecd6b8edd4dce0b248
Fabian Gutierrez <fagossa@gmail.com>
2016-10-18 21:59:17 +0200

    add error handling to PlanSource consumer example

--- scala/reactive-kafka-scala-example/src/main/scala/com/scalaio/kafka/consumer/PlainSource.scala
+++ scala/reactive-kafka-scala-example/src/main/scala/com/scalaio/kafka/consumer/PlainSource.scala
@@ -3,13 +3,15 @@ package com.scalaio.kafka.consumer
 import akka.actor.ActorSystem
 import akka.kafka.scaladsl.Consumer
 import akka.kafka.{ConsumerSettings, Subscriptions}
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.{Keep, Sink}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
 import com.scalaio.kafka.consumer.Settings.consumerSettings
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.Future
+import scala.util.control.NonFatal
 
 object Settings {
 
@@ -19,17 +21,44 @@ object Settings {
       .withGroupId("PlainSourceConsumer")
       .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
       .withProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+
+
 }
 
 object PlainSourceConsumerMain extends App {
+
+  lazy val logger = LoggerFactory.getLogger(getClass)
+
+  val decider: Supervision.Decider = {
+    case _: IllegalStateException => Supervision.Resume
+    case _ => Supervision.Stop
+  }
+
   implicit val system = ActorSystem("PlainSourceConsumerMain")
-  implicit val materializer = ActorMaterializer()
+  implicit val materializer = ActorMaterializer(
+    ActorMaterializerSettings(system)
+      .withSupervisionStrategy(decider)
+  )
 
   // plainSource, don't do auto commit by default!!!, see consumerSettings
   Consumer
     .plainSource(consumerSettings, Subscriptions.topics("topic1"))
     .mapAsync(1)(record => BusinessController.handleMessage(record.value()))
     .runWith(Sink.ignore)
+
+  // Handles failures
+  val (control, streamFuture) = Consumer
+    .plainSource(consumerSettings, Subscriptions.topics("topic1"))
+    .mapAsync(1)(record => BusinessController.handleMessage(record.value()))
+    .toMat(Sink.ignore)(Keep.both)
+    .run()
+
+  streamFuture.onFailure {
+    case NonFatal(e) => logger.error("error streaming", e)
+  }
+
+  val shutdownFuture = control.shutdown()
+
 }
 
 object BusinessController {
