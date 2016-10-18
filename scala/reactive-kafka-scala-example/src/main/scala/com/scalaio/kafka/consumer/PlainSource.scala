4c27fe3025a12c705fd0becbdb026e4e533fe376
Fabian Gutierrez <fagossa@gmail.com>
2016-10-18 21:27:53 +0200

    add example on Consumer.plainSource

--- scala/reactive-kafka-scala-example/src/main/scala/com/scalaio/kafka/consumer/PlainSource.scala
+++ scala/reactive-kafka-scala-example/src/main/scala/com/scalaio/kafka/consumer/PlainSource.scala
@@ -1,61 +1,41 @@
 package com.scalaio.kafka.consumer
 
-import java.util.concurrent.atomic.AtomicLong
-
-import akka.Done
 import akka.actor.ActorSystem
 import akka.kafka.scaladsl.Consumer
 import akka.kafka.{ConsumerSettings, Subscriptions}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
-import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
-import org.apache.kafka.common.TopicPartition
+import com.scalaio.kafka.consumer.Settings.consumerSettings
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+
+object Settings {
+
+  def consumerSettings(implicit system: ActorSystem) =
+    ConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
+      .withBootstrapServers("localhost:9092")
+      .withGroupId("PlainSourceConsumer")
+      .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+      .withProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+}
 
 object PlainSourceConsumerMain extends App {
   implicit val system = ActorSystem("PlainSourceConsumerMain")
   implicit val materializer = ActorMaterializer()
 
-  val consumerSettings = ConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
-    .withBootstrapServers("localhost:9092")
-    .withGroupId("PlainSourceConsumer")
-    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-
-  val db = new DB
-  db.loadOffset().foreach { fromOffset =>
-    val partition = 0
-    val subscription = Subscriptions.assignmentWithOffset(
-      new TopicPartition("topic1", partition) -> fromOffset
-    )
-    val done =
-      Consumer
-        .plainSource(consumerSettings, subscription)
-        .mapAsync(1)(db.save)
-        .runWith(Sink.ignore)
-  }
-
+  // plainSource, don't do auto commit by default!!!, see consumerSettings
+  Consumer
+    .plainSource(consumerSettings, Subscriptions.topics("topic1"))
+    .mapAsync(1)(record => BusinessController.handleMessage(record.value()))
+    .runWith(Sink.ignore)
 }
 
-class DB {
+object BusinessController {
+  type Service[A, B] = A => Future[B]
 
-  private val offset = new AtomicLong(2)
+  val handleMessage: Service[String, String] =
+    (message) => Future.successful(message.toUpperCase)
 
-  type KafkaMessage = ConsumerRecord[Array[Byte], String]
-
-  def save(record: KafkaMessage): Future[Done] = {
-    println(s"DB.save: ${record.value}")
-    offset.set(record.offset)
-    Future.successful(Done)
-  }
-
-  def loadOffset(): Future[Long] =
-    Future.successful(offset.get)
-
-  def update(data: String): Future[Done] = {
-    println(s"DB.update: $data")
-    Future.successful(Done)
-  }
 }
